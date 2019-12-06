38368beec45dc3bb1f0a29862d3032ec7cc9b572
Somchay <Somchayk@yahoo.com>
2019-12-05 19:59:26 -0700

    start inital project, create model and controllers, created static page and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
-Rails.application.routes.draw do
-  devise_for :billboards
+Rails.application.routes.draw do  
+  devise_for :users
+  root 'static#index'
+  
+  resources :billboards
+  resources :artists do
+    resources :songs
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
